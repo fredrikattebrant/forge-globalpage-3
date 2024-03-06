e8dc38428670dfd6587c823e8ac241361ff04746
fredrikattebrant <fredrik.attebrant@gmail.com>
2024-03-06 15:16:20 +0100

    Try with a build script

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USER="$1"
+KEY="$2"
+
+echo "Running the build script"
+
+npm ci
+npm install @forge/cli --no-optional
+npm forge settings set usage-analytics false
+
+cd static/hello-world
+npm ci
+ m run build
+cd ../..
+
+npx forge login -u $USER -t $KEY --non-interactive --verbose
+npx forge whoami # debuggin'
+npx forge deploy -e development
